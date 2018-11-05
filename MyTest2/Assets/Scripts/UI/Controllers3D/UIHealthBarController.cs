70da65e966cd66b4878e3d49cbabcc27a23e7230
elvess65 <37631917+elvess65@users.noreply.github.com>
2018-11-05 18:43:11 +0200

    1

---
+++ MyTest2/Assets/Scripts/UI/Controllers3D/UIHealthBarController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace mytest2.UI.Controllers3D
+{
+    public class UIHealthBarController : MonoBehaviour
+    {
+
+        // Use this for initialization
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
